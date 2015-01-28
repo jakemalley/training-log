fe24a9da3d28733bbb22b13302075eb9f878b8e1
Jake Malley <jja.malley@gmail.com>
2015-01-28 20:36:39 +0000

    Updated to add functionality to run the application in a tornado server. Used in production environments.

--- manage.py
+++ manage.py
@@ -22,20 +22,9 @@ except KeyError:
     # The environment variable was not set.
     # Assume we're in production.
     app.config.from_object('config.ProductionConfig')
-else:
-    # If we could get the config from the environment variable print it out.
-    print(environ['TRAINING_LOG_CONFIG'])
 
 manager = Manager(app)
-
-@manager.command
-def init_db():
-    from traininglog.models import Member
-    db.create_all()
-    db.session.commit()
-
 migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 
 # Get options from the config.
 try:
@@ -52,13 +41,63 @@ except KeyError:
     # If the option isn't present use the default.
     port=5000
 
-# Create the server to run the application.
+# Create a development server to run the application.
 server = Server(host=host, port=port)
 
+# Specify custom commands
+@manager.command
+def help():
+    """
+    Displays a simple help message.
+    """
+    print("""
+    To run the development server use $ python manage.py runserver
+    To run the production server use $ python manage.py tornadoserver
+        """)
+    
+@manager.command
+def init_db():
+    """
+    Create a new database as per the models specified
+    in the models.py file. Using the database file specified
+    in the config.py
+    """
+    from traininglog.models import Member
+    db.create_all()
+    db.session.commit()
+
+@manager.command
+def tornadoserver():
+    """
+    Create a tornado server to run the application 
+    (used in production).
+    """
+    # Import tornado for our tornado server.
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+    # Import signal used to stop tornado with ctrl-c.
+    import signal
+
+    # Define a callback to stop the server.
+    def stop_tornado(signum, frame):
+        # Stop the loop.
+        IOLoop.instance().stop()
+
+    signal.signal(signal.SIGINT, stop_tornado)
+    # Create the HTTP server and WSGI Container.
+    http_server = HTTPServer(WSGIContainer(app))
+    # Listen on the port specified in the config.
+    http_server.listen(port)
+    # Start the loop.
+    IOLoop.instance().start()
+
 # Add commands to the manager.
+# Add command for db migrations.
+manager.add_command('db', MigrateCommand)
 # Add the default runserver command for the application server.
 manager.add_command('runserver', server)
 
 if __name__ == "__main__":
-
+    # Run the script manager.
     manager.run()
