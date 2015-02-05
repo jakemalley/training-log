d59e01612476e2b9eb755ed76dcdfdf3dbe25c68
Jake Malley <jja.malley@gmail.com>
2015-02-05 14:25:06 +0000

    Updated dashboard to display actual exercise data and included a line and pie chart.

--- traininglog/dashboard/views.py
+++ traininglog/dashboard/views.py
@@ -10,6 +10,7 @@ from flask import flash, redirect, render_template, \
                 request, url_for, Blueprint
 from flask.ext.login import login_required, current_user
 from traininglog.models import Exercise
+from datetime import datetime
 
 # Setup the dashboard blueprint.
 dashboard_blueprint = Blueprint(
@@ -25,23 +26,79 @@ def dashboard():
     # Get all the exercise data.
     exercise_data = Exercise.query.filter_by(member=current_user).order_by(Exercise.id.desc()).limit(8).all()
 
-    chart_data = [{
-                    'value': 25,
+    # Data for the pie chart
+    pie_chart_data = [{
+                    'value': get_hours_running(),
                     'color':"#F7464A",
                     'highlight': "#FF5A5E",
                     'label': "Running"
                 },
                 {
-                    'value': 25,
+                    'value': get_hours_cycling(),
                     'color': "#46BFBD",
                     'highlight': "#5AD3D1",
                     'label': "Swimming"
                 },
                 {
-                    'value': 50,
+                    'value': get_hours_swimming(),
                     'color': "#FDB45C",
                     'highlight': "#FFC870",
                     'label': "Cycling"
                 }]
 
-    return render_template('dashboard.html',chart_data=chart_data, exercise_data=exercise_data)
+    # Data for the line chart.
+    line_chart_data = [data.get_calories_burned() for data in exercise_data][::-1]
+    # Empty labels for the line chart.
+    line_chart_labels = [data.get_datetime() for data in exercise_data][::-1]
+
+    # Get todays date.
+    now = datetime.utcnow()
+
+    # Get total exercised for this year.
+    exercise_total_year = get_exercise_total(datetime(now.year,1,1))
+    exercise_total_today = get_exercise_total(now)
+
+    progress_today_width = int(float(exercise_total_today/4)*100)
+    progress_year_width = int(float(exercise_total_today/1460)*100)
+
+    return render_template('dashboard.html',pie_chart_data=pie_chart_data, exercise_data=exercise_data,line_chart_data=line_chart_data,line_chart_labels=line_chart_labels,exercise_total_year=exercise_total_year,exercise_total_today=exercise_total_today,progress_today_width=progress_today_width,progress_year_width=progress_year_width)
+
+def get_hours_running():
+    """ 
+    Returns the total hours of running.
+    """
+    exercise_data = Exercise.query.filter_by(member=current_user,exercise_type='running').all()
+    total = 0
+    for data in exercise_data:
+        total += data.exercise_duration
+    return total
+
+def get_hours_cycling():  
+    """ 
+    Returns the total hours of cycling.
+    """ 
+    exercise_data = Exercise.query.filter_by(member=current_user,exercise_type='cycling').all()
+    total = 0
+    for data in exercise_data:
+        total += data.exercise_duration
+    return total
+
+def get_hours_swimming(): 
+    """ 
+    Returns the total hours of swimming.
+    """
+    exercise_data = Exercise.query.filter_by(member=current_user,exercise_type='swimming').all()
+    total = 0
+    for data in exercise_data:
+        total += data.exercise_duration
+    return total
+
+def get_exercise_total(date):
+    """
+    Returns the number of hours exercised on the date given.
+    """
+    exercise_data = Exercise.query.filter(Exercise.date>date.date()).filter_by(member=current_user).all()
+    total = 0
+    for data in exercise_data:
+        total += data.exercise_duration
+    return total
